a77fa2def273e89c82df0bca62a353bac5998099
Vivek Gawande <vivek040997@gmail.com>
2021-05-25 13:31:10 +0530

    Add project files.

---
+++ PCPartPriceTracker/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPartPriceTracker.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool InStock { get; set; }
+
+        public double TargetPrice { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
